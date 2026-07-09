7dd0986e97b22865a87b941f263a4260382867f8
Rishabh Patel <rishabhshailendrapatel@gmail.com>
2026-07-09 21:41:46 +0530

    Code: Pattern2

---
+++ Foundation/pattern2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class pattern2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num= sc.nextInt();
+
+        for(int i=num; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+Enter a number: 5
+*       *       *       *       *
+*       *       *       *
+*       *       *
+*       *
+*
+
+*/
